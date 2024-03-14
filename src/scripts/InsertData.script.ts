565e92cda8afa489437945c595f2f59d942dd694
lukaJokanovic <luka.jokanovic97@gmail.com>
2024-03-14 17:57:20 +0100

    lint

--- src/scripts/InsertData.script.ts
+++ src/scripts/InsertData.script.ts
@@ -1,13 +1,13 @@
-import { KinesisService } from "../services/external/Kinesis.service";
+import { KinesisService } from '../services/external/Kinesis.service';
 import events from './events.json';
-import { KinesisConfig } from "../configs/Kinesis.config";
+import { KinesisConfig } from '../configs/Kinesis.config';
 
 async function insertData(): Promise<void> {
-    const kinesisService = new KinesisService();
+  const kinesisService = new KinesisService();
 
-    for(const event of events){
-        await kinesisService.publish(KinesisConfig.USER_EVENTS_STREAM, event);
-    }
+  for (const event of events) {
+    await kinesisService.publish(KinesisConfig.USER_EVENTS_STREAM, event);
+  }
 }
 
 insertData();
